88e737b08322e4a1ee2a61d3a913c7dce2a054dd
ak4422920 <ak4422920@gmail.com>
2025-01-08 12:36:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7251898668').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "akteraboxdownloader_bot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Akforwardbot") 
 
     PICS = (environ.get('PICS', 'https://envs.sh/oob.jpg'))
     
@@ -15,7 +15,7 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL','-1002190352334'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/akmoviebotz") # FORCE SUB channel link 
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/akmovieshubbackup") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
 
